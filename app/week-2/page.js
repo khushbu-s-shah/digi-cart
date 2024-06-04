192ac53a4bff24538cc6994febd8b475365bb849
khushbu-s-shah <141067682+khushbu-s-shah@users.noreply.github.com>
2024-06-04 16:29:31 -0600

    Finish Week-2 work

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import Heading from "./heading.js";
+
+export default function Page () {
+    return (
+    <main>
+        <Heading />
+        <Heading />
+        <Heading />
+    </main>
+);
+}
